b814a5fdbf6ee2a3bcdcba1a886a52e831ff590e
Yuma Nemoto <nemoto1257@gmail.com>
2017-10-02 23:00:50 +0900

    add get-data from db

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,13 @@ var connection = require('../mysqlConnection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM boards';
+  connection.query(query, function(err, rows){
+    res.render('index', {
+      title: 'はじめてのNode.js',
+      boardList: rows
+    });
+  });
 });
 
 router.post('/', function(req, res, next){
