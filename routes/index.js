a8c582d7d5fcb094a2f50aa4414133cce7e8a51c
Yuma Nemoto <nemoto1257@gmail.com>
2017-10-02 22:44:58 +0900

    add db-post-function

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var connection = require('../mysqlConnection');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +10,13 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next){
   var title = req.body.title;
+  var createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  var query = 'INSERT INTO boards (title, created_at) VALUES ("' + title + '", ' + '"' + createdAt + '")';
+  connection.query(query, function(err, rows){
+    res.redirect('/');
+  });
   console.log(title);
+  console.log(createdAt);
 });
 
 module.exports = router;
